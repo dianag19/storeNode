bffe69c6cd962142956f2f9408af27c01cce1f21
Diana <dg@singularagency.co>
2023-05-29 20:19:30 -0500

    config vercel

--- api/index.js
+++ api/index.js
@@ -3,20 +3,23 @@ const cors = require('cors');
 const routerApi = require('./routes');
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-const whitelist = ['http://localhost:8080'];
+// const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+// const options = {
+//   origin: (origin,callback)=>{
+//     if(whitelist.includes(origin)){
+//       callback(null, true);
+//     }else{
+//       callback(new Error('No permitido'));
+//     }
+//   }
+// }
 const options = {
-  origin: (origin,callback)=>{
-    if(whitelist.includes(origin)){
-      callback(null, true);
-    }else{
-      callback(new Error('No permitido'));
-    }
-  }
-}
+  origin: "*"
+};
 app.use(cors(options));
 
 app.get('/api', (req, res) => {
